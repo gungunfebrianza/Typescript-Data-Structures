b5f4048efe846a201aea6cc08a081d492f25f08f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-07 06:41:46 +0700

    AddText

--- Object/dist/index.js
+++ Object/dist/index.js
@@ -1,22 +1,28 @@
 "use strict";
+var _a, _b;
 class NodeObject {
     constructor(val = 0) {
         this.left = null;
         this.right = null;
         this._root = null;
+        this.countAll = 0;
+        this.payload = "";
         this.value = val;
     }
     insert(newNodeValue) {
         const thisNode = new NodeObject(newNodeValue);
         if (!this._root) {
             this._root = thisNode;
+            this._root.payload = ` {"name" : "Root", "parent":"null"}`;
         }
         else {
-            var currentRoot = this._root;
+            let currentRoot = this._root;
+            currentRoot.countAll++;
             while (true) {
+                console.log(`compare current value ${currentRoot.value} with ${newNodeValue}`);
                 if (currentRoot.value > newNodeValue) {
                     if (currentRoot.left != null) {
-                        currentRoot = currentRoot.left; //4
+                        currentRoot = currentRoot.left;
                     }
                     else {
                         currentRoot.left = thisNode;
@@ -42,11 +48,15 @@ class NodeObject {
 const binarySearchTree = new NodeObject();
 //const t0 = performance.now(); // <-- Performance API Snapshot
 binarySearchTree.insert(30);
+binarySearchTree.insert(20);
 binarySearchTree.insert(37);
 binarySearchTree.insert(80);
+binarySearchTree.insert(12);
 binarySearchTree.insert(67);
+binarySearchTree.insert(3);
 binarySearchTree.insert(47);
 //const t1 = performance.now(); // <-- Performance API Snapshot
-console.log(binarySearchTree);
+console.log((_a = binarySearchTree._root) === null || _a === void 0 ? void 0 : _a.countAll);
+console.log((_b = binarySearchTree._root) === null || _b === void 0 ? void 0 : _b.countAll);
 //console.log(`Took ${t1 - t0} milliseconds.`); // <-- Calculate
 //# sourceMappingURL=index.js.map
--- Object/index.ts
+++ Object/index.ts
@@ -1,40 +1,58 @@
+import {
+  writeFileStr
+} from "https://deno.land/std/fs/mod.ts";
+
 type NodeType = null | NodeObject;
-type Counter = null | number;
 
 class NodeObject {
   private value: number;
   private left: NodeType = null;
   private right: NodeType = null;
-  private _root: NodeType = null;
-  private count: Counter = null;
+  public _root: NodeType = null;
+  public countAll: number = 0;
+  public payload: string = "";
 
   constructor(val: number = 0) {
     this.value = val;
-  }
-
-  initializer(rootnode: number): void {
-    this._root = new NodeObject(rootnode);
   }
 
   insert(newNodeValue: number): void {
     const thisNode: NodeObject = new NodeObject(newNodeValue);
     if (!this._root) {
       this._root = thisNode;
+      this._root.payload = ` {"name" : "Root", "parent":"null"}`;
+      writeFileStr("./target.dat", "file content");
     } else {
-      var currentRoot: NodeObject = this._root;
+      let currentRoot: NodeObject = this._root;
+      currentRoot.countAll++;
       while (true) {
+        console.log(
+          `compare current value ${currentRoot.value} with ${newNodeValue}`,
+        );
         if (currentRoot.value > newNodeValue) {
+          console.log(
+            `${newNodeValue} lesser than current value ${currentRoot.value}`,
+          );
           if (currentRoot.left != null) {
-            currentRoot = currentRoot.left; //4
+            currentRoot = currentRoot.left;
           } else {
             currentRoot.left = thisNode;
+            console.log(
+              `Add ${newNodeValue} to the left of ${currentRoot.value}`,
+            );
             break;
           }
         } else if (currentRoot.value < newNodeValue) {
+          console.log(
+            `${newNodeValue} bigger than current value ${currentRoot.value}`,
+          );
           if (currentRoot.right != null) {
             currentRoot = currentRoot.right;
           } else {
             currentRoot.right = thisNode;
+            console.log(
+              `Add ${newNodeValue} to the right of ${currentRoot.value}`,
+            );
             break;
           }
         } else {
@@ -47,12 +65,15 @@ class NodeObject {
 
 const binarySearchTree: NodeObject = new NodeObject();
 //const t0 = performance.now(); // <-- Performance API Snapshot
-binarySearchTree.initializer(20);
 binarySearchTree.insert(30);
+binarySearchTree.insert(20);
 binarySearchTree.insert(37);
 binarySearchTree.insert(80);
+binarySearchTree.insert(12);
 binarySearchTree.insert(67);
+binarySearchTree.insert(3);
 binarySearchTree.insert(47);
 //const t1 = performance.now(); // <-- Performance API Snapshot
-console.log(binarySearchTree);
+console.log(binarySearchTree._root?.countAll);
+console.log(binarySearchTree._root?.countAll);
 //console.log(`Took ${t1 - t0} milliseconds.`); // <-- Calculate
